5ab596483e9efec68382eae6b612d333d89582de
Shivam <shivam160291@gmail.com>
2018-09-21 20:11:04 +0530

    added & configured redux, react-redux, redux-saga, redux-logger etc

--- App.js
+++ App.js
@@ -1,49 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './src/store';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+export const store = configureStore();
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+class App extends React.Component {
+    render() {
+        return ( 
+        <SafeAreaView style={{ flex: 1, backgroundColor: '#f9f9f9' }}>
+            <Provider store={store}>
+                 {/* <ReduxNavigation /> */}
+                 <View><Text style={{ fontSize: 50 }}>Ayo</Text></View>
+            </Provider>
+        </SafeAreaView>
+        );
+    }
 }
+export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
